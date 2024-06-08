41931499d8815f482e92d62b9348ceec68a3f471
Adrian May - FW <sa.may@posteo.net>
2024-06-08 19:45:22 +0200

    FIrst prints and first function

--- src/audiolib/elac/elac.py
+++ src/audiolib/elac/elac.py
@@ -13,19 +13,19 @@ class Transducers(ABC):
 class ElectroDynamic(Transducers):
     def __init__(
             self,
-            Sd,
-            Mms,
-            Rec,
-            Lec,
-            Qts,
-            Qes,
-            Qms,
-            Cms,
-            Rms,
-            fs,
-            Bl,
+            Sd = None,
+            Mms = None,
+            Rec = None,
+            Lec = None,
+            Qts = None,
+            Qes = None,
+            Qms = None,
+            Cms = None,
+            Rms = None,
+            fs = None,
+            Bl = None,
     ):
-        self.Sd = Sd 
+        self.Sd = Sd
         self.Mms = Mms 
         self.Rec = Rec 
         self.Lec = Lec 
@@ -37,8 +37,23 @@ class ElectroDynamic(Transducers):
         self.fs = fs 
         self.Bl = Bl 
 
-    def imp_to_ts(self, f_z, z, ):
+    def imp_to_ts(self, f_z, z, print_ts, ):
         pass
 
     def ts_to_imp(self, f_min, f_max, ):
         pass
+
+    def print_ts(self):
+        print(79*'-')
+        print(f'Sd = {self.Sd}')
+        print(f'Mms = {self.Mms}')
+        print(f'Rec = {self.Rec}')
+        print(f'Lec = {self.Lec}')
+        print(f'Qts = {self.Qts}')
+        print(f'Qes = {self.Qes}')
+        print(f'Qms = {self.Qms}')
+        print(f'Cms = {self.Cms}')
+        print(f'Rms = {self.Rms}')
+        print(f'fs = {self.fs}')
+        print(f'Bl = {self.Bl}')
+        print(79*'-')
