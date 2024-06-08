dca4c32384e3616af9cb1dc8f594c60925bdf66f
Adrian May - FW <sa.may@posteo.net>
2024-06-08 15:14:31 +0200

    Default audiolib structure

---
+++ src/audiolib/elac/__init__.py
@@ -0,0 +1,15 @@
+# from .elac import (
+#     get_rfft_power_spec,
+#     get_rfft_spec,
+#     get_ir_from_rfft,
+#     get_ir_from_rawdata,
+#     get_msc,
+# )
+
+# __all__ = [
+#     'get_rfft_power_spec',
+#     'get_rfft_spec',
+#     'get_ir_from_rfft',
+#     'get_ir_from_rawdata',
+#     'get_msc',
+# ]
